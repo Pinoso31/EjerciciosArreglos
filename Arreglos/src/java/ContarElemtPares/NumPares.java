618079e68bc1cd1985eaf45af28832ea04ef8c67
Pinoso31 <jdespinozar@uamv.edu.ni>
2026-04-20 17:35:18 -0600

    Actualice el ejercicio de arreglos

--- Arreglos/src/java/ContarElemtPares/NumPares.java
+++ Arreglos/src/java/ContarElemtPares/NumPares.java
@@ -1,7 +1,19 @@
 package ContarElemtPares;
 
 public class NumPares {
-     static void main() {
+    static void main(String[] args) {
+        int [] numeros = {5, 12, 7, 25, 45, 8, 19, 33, 10, 22};
+         int suma = 0;
 
+         for ( int i = 0; i < numeros.length; i++) {
+             suma += numeros [i];
+         }
+
+        System.out.println("Arreglo de numero :");
+        for ( int num : numeros) {
+            System.out.println(num + " ");
+        }
+
+        System.out.println("\n\n La suma total de todos los elementos es : " + suma);
     }
 }
---
+++ Arreglos/src/test/java/NumPares.java
@@ -0,0 +1,22 @@
+package ContarElemtPares;
+
+public class NumPares {
+    //Crea un arreglo de números  Y escribe un programa que calcule
+         // la suma total de todos sus elementos.
+
+         //Arreglo: [5, 10, 15, 20]
+         // Resultado esperado: 50
+
+        static void main(String[] args) {
+            int[] numeros = {5, 10, 15, 20};
+
+            int suma = 0;
+
+            for (int i = 0; i < arreglo.length; i++){
+                suma += arreglo[i];
+            }
+
+            System.out.println("Arreglo : [5, 10, 15, 20]");
+            System.out.println("Resultado esperado : " + suma );
+       }
+}
